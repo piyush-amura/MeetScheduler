29f11edd249a86c4d4be713fa8cdc13f162f2ba9
peiyush13 <piyush.wani@amuratech.com>
2017-08-25 00:35:01 +0530

    created User model with devise gem along with Employee and Admin model

---
+++ app/models/admin.rb
@@ -0,0 +1,8 @@
+#
+# Class Admin provides <description>
+#
+# @author Joe Blog <Joe.Blog@nowhere.com>
+#
+class Admin < User
+
+end	
---
+++ app/models/employee.rb
@@ -0,0 +1,7 @@
+#
+# Class Employee provides <description>
+#
+# @author Joe Blog <Joe.Blog@nowhere.com>
+#
+class Employee < User
+end
