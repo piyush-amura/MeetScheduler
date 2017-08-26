6df3a44c3c2ef03e72ae3bf2aa411378ac9a1548
peiyush13 <piyush.wani@amuratech.com>
2017-08-26 22:31:53 +0530

    validation added for venue model on capacity and status

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,7 +3,27 @@
 # @author Piyush Wani <piyush.wani@amuratech.com>
 #
 class Venue < ApplicationRecord
+  TYPES = %w[available not_available].freeze
+
   has_many :meetings
+
+  validates :name, presence: true
+  validates :address, presence: true
+
+  # custom Validation for status
+  validate :status_validation, :capacity_validation
+
+  # method for checking status type
+  # adds a error to error array
+  def status_validation
+    errors.add(:status, 'restricted from use.') unless TYPES.include?(status)
+  end
+
+  # method for checking non negative value on capacity
+  # adds a error to error array
+  def capacity_validation
+    errors.add(:capacity, 'cannot be negative') if capacity <= 0
+  end
 
   # method for checking availability
   # of the venue
